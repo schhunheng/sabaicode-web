9acb2867b3ac4c17966c10e6540a7ce3321a5745
schhunheng <schhunheng.it@gmail.com>
2020-10-18 11:25:59 +0700

    client

---
+++ build/precache-manifest.c15b2587415313952008725a19cdd29b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e40c06ff7549a15228cb273f0785e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dab8473e048cc127728b",
+    "url": "/static/css/2.d7dd344d.chunk.css"
+  },
+  {
+    "revision": "6fe498cad07d844c3362",
+    "url": "/static/css/main.c500c98c.chunk.css"
+  },
+  {
+    "revision": "dab8473e048cc127728b",
+    "url": "/static/js/2.8c551c25.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.8c551c25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fe498cad07d844c3362",
+    "url": "/static/js/main.99db2e7c.chunk.js"
+  },
+  {
+    "revision": "132575fa147406d0b9f0",
+    "url": "/static/js/runtime-main.fa64f664.js"
+  }
+]);
